0c98db856657716dd0da487c7ce9f8d5a17aa75e
Jeegs <jhonngutierrez.contact@gmail.com>
2023-02-28 15:28:41 -0500

    sass path import solve and typo fixed

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,14 @@
 // import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import preprocess from "svelte-preprocess"
-import path from 'node:path'
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: preprocess({
     scss: {
-      prependData: `@import '${path.resolve('./src/styles/var.scss')}';`
+      // prependData: `@import '${path.resolve('./src/styles/var.scss')}';`
+      includePaths: ['src/styles'],
+      prependData: `@use 'src/styles/var' as *;`
     }
   }),
 }
